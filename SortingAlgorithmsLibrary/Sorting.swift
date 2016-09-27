592b7276c1bc5b23fbdc0e1008b3db042dac6971
Arturo Fernandez <aferathlon@gmail.com>
2016-09-26 23:23:03 -0700

    Initial Commit

---
+++ SortingAlgorithmsLibrary/Sorting.swift
@@ -0,0 +1,65 @@
+//
+//  Sorting.swift
+//  SortingAlgorithmsLibrary
+//
+//  Created by Arturo Fernandez on 9/17/16.
+//  Copyright © 2016 Arturo Fernandez. All rights reserved.
+//
+
+import Foundation
+
+class Sorting {
+    class func insertionSort(elements: [Int]) -> [Int] {
+        var tempElements: [Int] = elements
+         
+        for index in 1...elements.count - 1 {
+            var pivot: Int = index - 1
+            var movedIndex: Int = index
+            while( tempElements[movedIndex] < tempElements[pivot] ) {
+                swap(&tempElements[movedIndex], &tempElements[pivot])
+                movedIndex = pivot
+                if pivot > 0 {
+                    pivot = pivot - 1
+                }
+            }
+        }
+        
+        return tempElements
+    }
+    
+    class func selectionSort(elements: [Int]) -> [Int] {
+        var tempElements: [Int] = elements
+        var smallest: Int
+        var tempPivot: Int
+        for index in 0...elements.count - 2 {
+            smallest = tempElements[index]
+            tempPivot = index
+            for pivot in index + 1...elements.count - 1 {
+                if smallest > tempElements[pivot] {
+                    smallest = tempElements[pivot]
+                    tempPivot = pivot
+                }
+            }
+            let temp: Int = tempElements[index]
+            tempElements[index] = smallest
+            tempElements[tempPivot] = temp
+        }
+        
+        return tempElements
+    }
+    
+    class func printArray(elements: [Int], inOrder: Bool) {
+        if inOrder == true {
+            for index in 0...elements.count - 1 {
+                print("\(elements[index]), ")
+            }
+        } else {
+            for index in elements.count - 1...0 {
+                print("\(elements[index]), ")
+            }
+        }
+    }
+}
+
+
+
---
+++ SortingAlgorithmsLibrary/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  SortingAlgorithmsLibrary
+//
+//  Created by Arturo Fernandez on 9/17/16.
+//  Copyright © 2016 Arturo Fernandez. All rights reserved.
+//
+
+import Foundation
+
+let testArray: [Int] = [22, 4, 1, 10, 5, 3, 10, 7, 20]
+
+print(Sorting.insertionSort(elements: testArray))
+print(Sorting.selectionSort(elements: testArray))
+
+
+
